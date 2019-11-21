c0e50b947969932f0f2bafd1b5997fcc04bff279
PriyankaPawar <PriyankaPawar@9.199.36.219>
2019-11-21 17:41:03 +0530

    code changes

--- final-framework-testng/tests/com/training/sanity/tests/RealestateRegisterTests.java
+++ final-framework-testng/tests/com/training/sanity/tests/RealestateRegisterTests.java
@@ -12,7 +12,7 @@ import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.Test; 
 
 import com.training.generics.GenericMethods;
 import com.training.generics.ScreenShot;
